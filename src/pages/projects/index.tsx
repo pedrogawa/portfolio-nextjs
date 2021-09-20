0d8017660e848bcf8732d69d578dcc25ae1e0fd6
Pedro <phfbarreiros95@gmail.com>
2021-09-20 00:33:12 -0300

    Teste commit

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -37,6 +37,7 @@ export default function Projects({ projects }: ProjectsProps) {
           <title>Projetos</title>
         </Head>
         <ProjectsContainer>
+          <span>Teste</span>
           {projects.map(project => {
             return (
               <React.Fragment key={project.slug}>
